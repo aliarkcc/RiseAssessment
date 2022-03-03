7768da40b92d85f4a1508cac251a545fdc360b2c
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:37:16 +0300

    DataAccess Katmanında Abstract Klasörü İçerisinde IContactInfoDal ve IDirectoryDal oluşturuldu.

---
+++ DataAccess/Abstract/IContactInfoDal.cs
@@ -0,0 +1,13 @@
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract
+{
+    public interface IContactInfoDal:IBaseRepository<ContactInfo>
+    {
+        List<ContactInfoDto> GetDirectoryDetailDto(int id);
+    }
+}
