133db73884352ee707c6bf3fba8b9883844514f3
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:33:00 +0300

    Core Katmanında Entities Klasöründe Veritabanı işlemleri için Generic Repository oluşturuldu

---
+++ Core/DataAccess/EntityFramework/EfBaseRepository.cs
@@ -0,0 +1,67 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework
+{
+    public class EfBaseRepository<TEntity, TContext> : IBaseRepository<TEntity> where TEntity : class, IEntity, new() where TContext : DbContext, new()
+    {
+
+        async public Task<TEntity> AddAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                await context.Set<TEntity>().AddAsync(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+
+        async public Task<bool> DeleteAsync(int id)
+        {
+            using (TContext context = new TContext())
+            {
+                var deletedEntity = await context.Set<TEntity>().FindAsync(id);
+                context.Set<TEntity>().Remove(deletedEntity);
+                var data = await context.SaveChangesAsync();
+                if (data > 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+        async public Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter)
+        {
+            using (TContext context = new TContext())
+            {
+                return await context.Set<TEntity>().SingleOrDefaultAsync(filter);
+            }
+        }
+
+        public async Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity, bool>> filter = null)
+        {
+            using (TContext context = new TContext())
+            {
+                return filter == null
+                    ? await context.Set<TEntity>().ToListAsync()
+                    : await context.Set<TEntity>().Where(filter).ToListAsync();
+            }
+        }
+
+        async public Task<TEntity> UpdateAsync(TEntity entity)
+        {
+            using (TContext context = new TContext())
+            {
+                context.Set<TEntity>().Update(entity);
+                await context.SaveChangesAsync();
+                return entity;
+            }
+        }
+    }
+}
