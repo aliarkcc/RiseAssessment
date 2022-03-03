942be93f69c870046004b9089c4e0c7a25bc4d2d
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:38:28 +0300

    Business Katmanında Abstract Klasörü İçerisinde IContactInfoService ve IDirectoryService oluşturuldu.

---
+++ Business/Abstract/IContactInfoService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using Entities.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IContactInfoService
+    {
+        Task<ContactInfo> AddAsync(ContactInfo contactInfo);
+        Task<bool> DeleteAsync(int id);
+        Task<ContactInfo> GetByIdAsync(int id);
+        Task<IEnumerable<ContactInfo>> GetListAsync();
+        Task<ContactInfo> UpdateAsync(ContactInfo contactInfo);
+        List<ContactInfoDto> GetDirectoryDetailDto(int id);
+    }
+}
---
+++ Business/Abstract/IDirectoryService.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IDirectoryService
+    {
+        Task<Directory> AddAsync(Directory directory);
+        Task<bool> DeleteAsync(int id);
+        Task<Directory> GetByIdAsync(int id);
+        Task<IEnumerable<Directory>> GetListAsync();
+        Task<Directory> UpdateAsync(Directory directory);
+    }
+}
