8608c7fa6f8e8178e43ddf90de29e880c6bbb047
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:39:52 +0300

    Web Apiye ContactInfosController ve DirectoriesController Eklendi.

---
+++ WebAPI/Controllers/ContactInfosController.cs
@@ -0,0 +1,77 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactInfosController : ControllerBase
+    {
+
+        IContactInfoService _contactInfoService;
+
+        public ContactInfosController(IContactInfoService contactInfoService)
+        {
+            _contactInfoService = contactInfoService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _contactInfoService.GetListAsync();
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        //public async Task<IActionResult> GetById(int id)
+        //{
+        //    var result = await _contactInfoService.GetByIdAsync(id);
+        //    if (result != null)
+        //        return Ok(result);
+        //    return BadRequest();
+        //}
+        public IActionResult GetByIdDetails(int id)
+        {
+            var result =  _contactInfoService.GetDirectoryDetailDto(id);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] ContactInfo contactInfo)
+        {
+            var result = await _contactInfoService.AddAsync(contactInfo);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _contactInfoService.DeleteAsync(id);
+            if (result)
+                return Ok(true);
+            return BadRequest(false);
+        }
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] ContactInfo contactInfo)
+        {
+            var result = await _contactInfoService.UpdateAsync(contactInfo);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+    }
+}
---
+++ WebAPI/Controllers/DirectoriesController.cs
@@ -0,0 +1,69 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DirectoriesController : ControllerBase
+    {
+        IDirectoryService _directoryService;
+
+        public DirectoriesController(IDirectoryService directoryService)
+        {
+            _directoryService = directoryService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _directoryService.GetListAsync();
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpGet]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var result = await _directoryService.GetByIdAsync(id);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<IActionResult> Add([FromBody] Directory directory)
+        {
+            var result = await _directoryService.AddAsync(directory);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+        [HttpDelete]
+        [Route("[action]/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var result = await _directoryService.DeleteAsync(id);
+            if (result)
+                return Ok(true);
+            return BadRequest(false);
+        }
+        [HttpPut]
+        [Route("[action]")]
+        public async Task<IActionResult> Update([FromBody] Directory directory)
+        {
+            var result = await _directoryService.UpdateAsync(directory);
+            if (result != null)
+                return Ok(result);
+            return BadRequest();
+        }
+    }
+}
