22f746f95914ac7debac560308ff58c5b767e90d
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:35:57 +0300

    Entities Katmanında Dto Klasörü içerisinde Detaylı iletişim bilgisi View Modeli Oluşturuldu.

---
+++ Entities/Dto/ContactInfoDto.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using Entities.Concrete;
+using System.Collections.Generic;
+
+namespace Entities.Dto
+{
+    public class ContactInfoDto:IDto
+    {
+        public int DirectoryId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+        public List<ContactInfo> ContactInfos{ get; set; }
+    }
+}
