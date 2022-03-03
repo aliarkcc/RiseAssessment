185128474cbc0a30140518edcb7d409baed3033d
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:34:47 +0300

    Entities Katmanında Concrete Klasörü içinde Entity classları oluşturuldu.

---
+++ Entities/Concrete/ContactInfo.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class ContactInfo:IEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        public int DirectoryId { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Entities/Concrete/Directory.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Concrete
+{
+    public class Directory:IEntity
+    {
+        [Key]
+        public int DirectoryId { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Company { get; set; }
+    }
+}
