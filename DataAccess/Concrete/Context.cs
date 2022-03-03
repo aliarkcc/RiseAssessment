39e5be8e757a4dfbbac41d127e042f66ebc14035
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:37:48 +0300

    DataAccess Katmanı Concrete içerisinde Context nesnesi oluşturuldu.

---
+++ DataAccess/Concrete/Context.cs
@@ -0,0 +1,15 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MEHMETPC\\SQLEXPRESS;database=RiseConsultingWork;integrated security=true");
+        }
+        public DbSet<ContactInfo> ContactInfos{ get; set; }
+        public DbSet<Directory> Directories{ get; set; }
+    }
+}
