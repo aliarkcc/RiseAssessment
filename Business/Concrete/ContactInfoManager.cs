486933d27db71192f5b5023d02571ad9c25e6049
mehmet ali <alimehmet1@windowslive.com>
2022-03-04 00:38:55 +0300

    Business Katmanında Concrete Klasörü İçerisinde ContactInfoManager ve DirectoryManager oluşturuldu.

---
+++ Business/Concrete/ContactInfoManager.cs
@@ -0,0 +1,49 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class ContactInfoManager : IContactInfoService
+    {
+        IContactInfoDal _contactInfoDal;
+
+        public ContactInfoManager(IContactInfoDal contactInfoDal)
+        {
+            _contactInfoDal = contactInfoDal;
+        }
+
+        public async Task<ContactInfo> AddAsync(ContactInfo contactInfo)
+        {
+            return await _contactInfoDal.AddAsync(contactInfo);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _contactInfoDal.DeleteAsync(id);
+        }
+
+        public async Task<ContactInfo> GetByIdAsync(int id)
+        {
+            return await _contactInfoDal.GetAsync(x => x.DirectoryId == id);
+        }
+
+        public List<ContactInfoDto> GetDirectoryDetailDto(int id)
+        {
+            return _contactInfoDal.GetDirectoryDetailDto(id);
+        }
+
+        public async Task<IEnumerable<ContactInfo>> GetListAsync()
+        {
+            return await _contactInfoDal.GetListAsync();
+        }
+
+        public async Task<ContactInfo> UpdateAsync(ContactInfo contactInfo)
+        {
+            return await _contactInfoDal.UpdateAsync(contactInfo);
+        }
+    }
+}
---
+++ Business/Concrete/DirectoryManager.cs
@@ -0,0 +1,43 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class DirectoryManager : IDirectoryService
+    {
+        IDirectoryDal _directoryDal;
+
+        public DirectoryManager(IDirectoryDal directoryDal)
+        {
+            _directoryDal = directoryDal;
+        }
+
+        public async Task<Directory> AddAsync(Directory directory)
+        {
+            return await _directoryDal.AddAsync(directory);
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            return await _directoryDal.DeleteAsync(id);
+        }
+
+        public async Task<Directory> GetByIdAsync(int id)
+        {
+            return await _directoryDal.GetAsync(x=>x.DirectoryId==id);
+        }
+
+        public async Task<IEnumerable<Directory>> GetListAsync()
+        {
+            return await _directoryDal.GetListAsync();
+        }
+
+        public async Task<Directory> UpdateAsync(Directory directory)
+        {
+            return await _directoryDal.UpdateAsync(directory);
+        }
+    }
+}
